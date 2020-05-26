1319a77b42769bf1c46eea826f8d07b97274c77d
Colin Bingham <cbingham@internal.tecore.com>
2020-05-26 14:56:05 -0400

    Fixed Rotating X in footer

--- scripts/rotate-footer.js
+++ scripts/rotate-footer.js
@@ -1,12 +1,9 @@
 var coll = document.getElementsByClassName("column-section");
-var activeIcon = window.getComputedStyle(document.querySelector('.column-section-title'), ':after').getPropertyValue('content');
 var i;
 
 for (i=0; i < coll.length; i++){
     coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
         var content = this.childNodes;
-        console.log(activeIcon);
         if (content[3].style.maxHeight){
             content[3].style.maxHeight = null;
           } 
@@ -16,4 +13,14 @@ for (i=0; i < coll.length; i++){
   });
 }
 
-
+for (i=0; i < coll.length; i++){
+    coll[i].addEventListener("click", function(){
+        var activeIcon = this.querySelector('.column-section-title');
+        if (activeIcon.classList.contains("active")){
+            activeIcon.classList.remove("active");
+        }
+        else {
+            activeIcon.classList.add("active");
+        }
+    });
+}
