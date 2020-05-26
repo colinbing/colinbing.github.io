260f26f386e2df1344803b66c1fe061cb7a116a5
Colin Bingham <cbingham@internal.tecore.com>
2020-05-26 17:26:03 -0400

    Added Menu Search

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,19 +1,19 @@
 var menuIcon = document.querySelector('.icon');
 var body = document.querySelector('body');
+var menu = document.querySelector('menu-clicked');
 
-document.addEventListener("click", function() {
+menuIcon.addEventListener("click", function() {
     menuIcon.classList.toggle('menu-active');
     console.log(menuIcon);
 });
 
-document.addEventListener("click", function() {
+menuIcon.addEventListener("click", function() {
     if (menuIcon.classList.contains('menu-active')){
-        console.log("Body is locked")
         body.classList.add('lock-scroll');
+        menu.classList.add('menu-visible');
     }
     else {
         body.classList.remove('lock-scroll');
-        console.log("Body is free")
     }
 });
 
