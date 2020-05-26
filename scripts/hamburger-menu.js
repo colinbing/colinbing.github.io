46eadd0c08e9cbadd283d4c82fe2f7cc40a5f9ad
Colin Bingham <cbingham@internal.tecore.com>
2020-05-26 16:48:25 -0400

    Locked scrollbar when menu is active

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,5 +1,19 @@
 var menuIcon = document.querySelector('.icon');
+var body = document.querySelector('body');
 
 document.addEventListener("click", function() {
-    menuIcon.classList.toggle('menu-active')    
+    menuIcon.classList.toggle('menu-active');
+    console.log(menuIcon);
 });
+
+document.addEventListener("click", function() {
+    if (menuIcon.classList.contains('menu-active')){
+        console.log("Body is locked")
+        body.classList.add('lock-scroll');
+    }
+    else {
+        body.classList.remove('lock-scroll');
+        console.log("Body is free")
+    }
+});
+
