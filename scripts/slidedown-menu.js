9101e357476b1cbe0fb66ab2eb71050c2dbda23a
colinbing <65261757+colinbing@users.noreply.github.com>
2020-05-27 01:34:01 -0400

    Added formatting for dropdown menu

--- scripts/slidedown-menu.js
+++ scripts/slidedown-menu.js
@@ -1,11 +1,15 @@
 var menuIcon = document.querySelector('.icon');
 var menu = document.querySelector('.menu-clicked');
+var menuContents = document.querySelectorAll('.menu-clicked div');
+var navMenu = document.querySelector('#navbar-mobile');
 
 menuIcon.addEventListener("click", function() {
     if (menuIcon.classList.contains('menu-active')){
         menu.classList.add('menu-visible');
+        navMenu.classList.add('nav-active')
     }
     else {
         menu.classList.remove('menu-visible');
+        navMenu.classList.remove('nav-active')
     }
 });
