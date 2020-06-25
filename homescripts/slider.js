718eaadb22c8f6055c0d9540119d5f17ffbe5b36
Colin Bingham <cbingham@internal.tecore.com>
2020-06-25 16:46:57 -0400

    Fixed Slider and reformatted rows. Added padding to socials

--- homescripts/slider.js
+++ homescripts/slider.js
@@ -1,20 +1,55 @@
-$('#carouselExample').on('slide.bs.carousel', function (e) {
+var $carousel = jQuery('.carousel-onebyone .carousel');
+if($carousel.length){
+	jQuery('.carousel-onebyone').on('slide.bs.carousel', carousel_onebyone);
+	carousel_set($carousel);
+	var resizeId;
+	jQuery(window).resize(function() {
+		clearTimeout(resizeId);
+		resizeId = setTimeout(()=>carousel_set($carousel), 500);
+	});	
+}
 
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $('.carousel-item').length;
-    
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-});
+function carousel_set($carousel){
+	if(!$carousel || !$carousel.length) return;
+	
+	$carousel.each((i, el)=>{
+		var $el = jQuery(el);
+		var itemsPerSlide = carousel_itemsPerSlide($el);
+		var totalItems = $el.find('.carousel-item').length;
+
+		if(itemsPerSlide < totalItems){
+			$el.find('.carousel-control').removeClass('hidden');
+		}else{
+			$el.find('.carousel-control').addClass('hidden');
+		}
+	});
+}
+
+function carousel_onebyone(e){
+	var carouselID = '#'+jQuery(this).find('.carousel').attr('id');
+	var $carousel = jQuery(carouselID);
+	var $inner = $carousel.find('.carousel-inner');
+	var $items = $carousel.find('.carousel-item');
+
+	var idx = jQuery(e.relatedTarget).index();
+	var itemsPerSlide = carousel_itemsPerSlide($carousel);
+	var totalItems = $items.length;
+	
+	if (idx >= totalItems-(itemsPerSlide-1)) {
+		var it = itemsPerSlide - (totalItems - idx);
+		for (var i=0; i<it; i++) {
+			if (e.direction == 'left') {
+				$items.eq(i).appendTo($inner);
+			}else {
+				$items.eq(0).appendTo($inner);
+			}
+		}
+	}
+}
+
+function carousel_itemsPerSlide($carousel){
+	var itemW = $carousel.find('.carousel-item').width();
+	var innerW = $carousel.find('.carousel-inner').width();
+	
+	return Math.floor(innerW/itemW);
+}
