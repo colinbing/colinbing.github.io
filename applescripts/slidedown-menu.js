2c12637398bc718298739b3cf6cfee0c73b3c2b4
colinbing <65261757+colinbing@users.noreply.github.com>
2020-07-24 13:39:32 -0400

    Working onh Apple Searchbar

--- applescripts/slidedown-menu.js
+++ applescripts/slidedown-menu.js
@@ -3,13 +3,16 @@ var menu = document.querySelector('.menu-clicked');
 var menuContents = document.querySelectorAll('.menu-clicked div');
 var navMenu = document.querySelector('#navbar-mobile');
 var dropDown = document.querySelectorAll('.dropdown-options li')
+var dropDownSearch = document.querySelector('.search-container')
+var dropDownHR = document.querySelector('.dropdown-content')
 
 menuIcon.addEventListener("click", function() {
     if (menuIcon.classList.contains('menu-active')){
         menu.classList.add('menu-visible');
         navMenu.classList.add('nav-active');
+        dropDownSearch.classList.add('dropdown-anim');
+        dropDownHR.classList.add('dropdown-anim');
         [].forEach.call(dropDown, function(el) {
-            el.classList.remove('dropdown-hide');
             el.classList.add('dropdown-anim');
         });
         
@@ -17,8 +20,9 @@ menuIcon.addEventListener("click", function() {
     else {
         menu.classList.remove('menu-visible');
         navMenu.classList.remove('nav-active');
+        dropDownSearch.classList.remove('dropdown-anim');
+        dropDownHR.classList.remove('dropdown-anim');
         [].forEach.call(dropDown, function(el) {
-            el.classList.add('dropdown-hide');
             el.classList.remove('dropdown-anim');
         });
         
