fd64dc444463274a08a5cde62c1f79478423ca31
colinbing <65261757+colinbing@users.noreply.github.com>
2020-07-24 15:13:03 -0400

    So close to apple menu being fixed

--- applescripts/slidedown-menu.js
+++ applescripts/slidedown-menu.js
@@ -2,18 +2,23 @@ var menuIcon = document.querySelector('.icon');
 var menu = document.querySelector('.menu-clicked');
 var menuContents = document.querySelectorAll('.menu-clicked div');
 var navMenu = document.querySelector('#navbar-mobile');
-var dropDown = document.querySelectorAll('.dropdown-options li')
-var dropDownSearch = document.querySelector('.search-container')
-var dropDownHR = document.querySelector('.dropdown-content')
+var dropDownList = Array.from (document.querySelectorAll('.dropdown-options li'));
+var index=0;
+var dropDownSearch = document.querySelector('.search-container');
+console.log(dropDownSearch);
 
 menuIcon.addEventListener("click", function() {
     if (menuIcon.classList.contains('menu-active')){
         menu.classList.add('menu-visible');
         navMenu.classList.add('nav-active');
         dropDownSearch.classList.add('dropdown-anim');
-        dropDownHR.classList.add('dropdown-anim');
-        [].forEach.call(dropDown, function(el) {
+        /* [].forEach.call(dropDown, function(el) {
             el.classList.add('dropdown-anim');
+        }); */
+        dropDownList.forEach((arrayElement, index) => {
+        setTimeout(function(){
+            dropDownList[index].className  += " dropdown-anim";
+        }, 30*(index+1));
         });
         
     }
@@ -21,10 +26,9 @@ menuIcon.addEventListener("click", function() {
         menu.classList.remove('menu-visible');
         navMenu.classList.remove('nav-active');
         dropDownSearch.classList.remove('dropdown-anim');
-        dropDownHR.classList.remove('dropdown-anim');
-        [].forEach.call(dropDown, function(el) {
+       [].forEach.call(dropDownList, function(el) {
             el.classList.remove('dropdown-anim');
-        });
+        }); 
         
     }
 });
